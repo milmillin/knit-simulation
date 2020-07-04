5b2a09a7b0ca8d13221587cf5b02930b35974e3e
Tom Lou <louyu27@cs.washington.edu>
2020-07-04 13:41:30 -0600

    Use parallel transform

--- ui/sweep.cpp
+++ ui/sweep.cpp
@@ -9,8 +9,8 @@
 static Eigen::MatrixXf circle(glm::vec3 offset, glm::vec3 d1, glm::vec3 d2, float radius, int stride) {
   Eigen::MatrixXf vertices(stride, 3);
   for (int i = 0; i < stride; i++) {
-    glm::vec3 vertex = glm::sin((float)i * 2 * glm::pi<float>() / stride) * d1 
-                     + glm::cos((float)i * 2 * glm::pi<float>() / stride) * d2
+    glm::vec3 vertex = (glm::sin((float)i * 2 * glm::pi<float>() / stride) * d1 
+                        + glm::cos((float)i * 2 * glm::pi<float>() / stride) * d2) * radius
                      + offset;
     vertices(i, 0) = vertex.x;
     vertices(i, 1) = vertex.y;
@@ -19,39 +19,16 @@ static Eigen::MatrixXf circle(glm::vec3 offset, glm::vec3 d1, glm::vec3 d2, floa
   return vertices;
 }
 
-static Eigen::MatrixXf circleEnd(glm::vec3 offset, glm::vec3 normal, float radius, int stride) {
-  glm::vec3 d1;
-  if (glm::abs(normal.x + normal.y) < glm::abs(normal.z)) {
-    d1 = glm::cross(glm::vec3(1, 0, 0), normal);
-  } else {
-    d1 = glm::cross(glm::vec3(0, 0, 1), normal);
-  }
-  glm::vec3 d2 = glm::cross(normal, d1);
-
-  d1 = glm::normalize(d1) * radius;
-  d2 = glm::normalize(d2) * radius;
-
-  return circle(offset, d1, d2, radius, stride);
-}
-
-static Eigen::MatrixXf circleJoint(glm::vec3 offset, glm::vec3 dir1, glm::vec3 dir2, float radius, int stride) {
-  dir1 = glm::normalize(dir1);
-  dir2 = glm::normalize(dir2);
-
-  if (glm::length(dir1 + dir2) < 0.001) {
-    return circleEnd(offset, dir1, radius, stride);
-  }
-
-  glm::vec3 d1 = glm::normalize(dir1 + dir2);
-  glm::vec3 d2 = glm::cross(dir1, dir2);
-  return circle(offset, d1, d2, radius, stride);
+static void parallelTransform(glm::vec3 tangent, glm::vec3 oldUp, glm::vec3 &newUp, glm::vec3 &newCross) {
+  newCross = glm::normalize(glm::cross(tangent, oldUp));
+  newUp = glm::normalize(glm::cross(newCross, tangent));
 }
 
 void circleSweep(const Eigen::MatrixXf path, const float radius,
     Eigen::MatrixXf &vertex, Eigen::MatrixXi &triangles,
     const int stride) {
-  // const int nPoints = path.rows();
-  const int nPoints = 4;
+  const int nPoints = path.rows();
+  // const int nPoints = 100;
 
   if (nPoints <= 1) {
     vertex = Eigen::MatrixXf(0, 3);
@@ -64,22 +41,37 @@ void circleSweep(const Eigen::MatrixXf path, const float radius,
 
   glm::vec3 startPoint = POINT_FROM_ROW(path, 0);
   glm::vec3 nextPoint = POINT_FROM_ROW(path, 1);
+  glm::vec3 tangent = nextPoint - startPoint;
+  glm::vec3 up;
+  if (glm::abs(tangent.x + tangent.y) < glm::abs(tangent.z)) {
+    up = glm::cross(glm::vec3(1, 0, 0), tangent);
+  } else {
+    up = glm::cross(glm::vec3(0, 0, 1), tangent);
+  }
+  up = glm::normalize(up);
+  glm::vec3 cross = glm::normalize(glm::cross(tangent, up));
+
   vertex.block(0, 0, stride, 3) = \
-    circleEnd(startPoint, nextPoint - startPoint, radius, stride);
+    circle(startPoint, up, cross, radius, stride);
   
-  for (int i = 1; i < nPoints - 1; i++) {
-    glm::vec3 prevPoint = POINT_FROM_ROW(path, i - 1);
-    glm::vec3 currentPoint = POINT_FROM_ROW(path, i);
-    glm::vec3 nextPoint = POINT_FROM_ROW(path, i + 1);
+  for (int i = 1; i < nPoints; i++) {
+    glm::vec3 currentPoint;
+    glm::vec3 nextPoint;
+    // TODO: better handler the end point
+    if (i == nPoints - 1) {
+      currentPoint = POINT_FROM_ROW(path, i - 1);
+      nextPoint = POINT_FROM_ROW(path, i);
+    } else {
+      currentPoint = POINT_FROM_ROW(path, i);
+      nextPoint = POINT_FROM_ROW(path, i + 1);
+    }
+    parallelTransform(nextPoint - currentPoint, up, up, cross);
 
     vertex.block(i * stride, 0, stride, 3) = 
-      circleJoint(currentPoint, prevPoint - currentPoint, nextPoint - currentPoint,
+      circle(currentPoint, up, cross,
                   radius, stride);
   }
   glm::vec3 endPoint = POINT_FROM_ROW(path, nPoints - 1);
-  glm::vec3 prevPoint = POINT_FROM_ROW(path, nPoints - 2);
-  vertex.block((nPoints - 1) * stride, 0, stride, 3) = \
-    circleEnd(endPoint, endPoint - prevPoint, radius, stride);
   
   ROW_FROM_POINT(vertex, vertex.rows() - 2, startPoint);
   ROW_FROM_POINT(vertex, vertex.rows() - 1, endPoint);
