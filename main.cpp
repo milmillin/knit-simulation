fcbd9fef3b8db4ae514db80734ffeb30b7e89752
Milin Kodnongbua <mil.millin@hotmail.com>
2020-07-06 13:23:07 -0700

    Load file from args

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "ui/viewer.h"
 
 int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " yarns-filename" << std::endl;
+    return -1;
+  }
+
   // Launch viewer
   UI::Viewer viewer;
-  viewer.loadYarn("../helloworld.yarns");
+  viewer.loadYarn(argv[1]);
   viewer.launch();
 }
